8284e22e2efc3c351d60d5a3b885e62572aa1364
moorebioinfo <matthew.p.moore@warwick.ac.uk>
2022-06-26 14:47:18 +0100

    Change pairwise comparisons to I/O management of sequences

--- FlexCore.py
+++ FlexCore.py
@@ -7,7 +7,6 @@ from itertools import repeat
 import concurrent.futures
 from concurrent.futures import ProcessPoolExecutor
 
-
 def add_args(a):
     parser = argparse.ArgumentParser(description=''' Test description ''')
     parser.add_argument('--alignment', '-a', help='Provide path and filename of alignment', required=True)
@@ -18,29 +17,29 @@ def add_args(a):
     args = parser.parse_args(a)
     return(args)
 
-def get_pw_SNPs(pairs, threshold, popsize, seqdict):
-
+def get_pw_SNPs(pairs, popsize, coreseqindex):
     results =[]
-
     print('Running pairwise...')
-
     def delete_not_pw_shared(mylist, idx):
         myarray = np.array(mylist)
         mask = np.full(len(mylist), True, dtype=bool)
         mask[idx] = False
         complement = list(myarray[mask])
         return(complement)
-
     for pair in pairs:
-        #print(pair)
         g1 = pair.split(',')[0]
         g2 = pair.split(',')[1]
-        g1seqlist = seqdict.get(g1)
-        g2seqlist = seqdict.get(g2)
-
+        g1line = coreseqindex.get(g1)
+        g2line = coreseqindex.get(g2)
+        filehandle = open('Coresites.fasta')
+        g1seqlist=list(filehandle.readlines()[g1line].rstrip())
+        filehandle.close()
+        filehandle = open('Coresites.fasta')
+        g2seqlist=list(filehandle.readlines()[g2line].rstrip())
+        filehandle.close()
+        #get sites missing in either seq of pair
         gapindex = list(set([i for i, e in enumerate(g1seqlist) if e == '-'] + [i for i, e in enumerate(g2seqlist) if e == '-']))
         gapindex += list(set([i for i, e in enumerate(g1seqlist) if e == 'N'] + [i for i, e in enumerate(g2seqlist) if e == 'N']))
-
         if len(gapindex)>0:
             g1nuc = delete_not_pw_shared(g1seqlist, gapindex)
             g2nuc = delete_not_pw_shared(g2seqlist, gapindex)
@@ -48,43 +47,28 @@ def get_pw_SNPs(pairs, threshold, popsize, seqdict):
             g1nuc = g1seqlist
             g2nuc = g2seqlist
         SNPcount = len(g1nuc) - sum(x == y for x, y in zip(g1nuc, g2nuc))
-
         sharedseq = len(g1nuc)-len(gapindex)
-
-        fSNP = SNPcount/len(g1nuc)
-        results.append((f'{g1},{g2},{SNPcount},{sharedseq},{fSNP}\n'))
+        SNPdist = SNPcount/len(g1nuc)
+        corSNP = SNPdist*(len(g1seqlist))
+        results.append((f'{g1},{g2},{SNPcount},{sharedseq},{SNPdist},{corSNP}\n'))
         #print(f'{g1},{g2},{SNPcount},{sharedseq},{fSNP}\n')
     return(results)
 
 
-
-
 def get_core(filename, percentcore, popsize):
-
-    print(filename)
-
     indexdict ={}
-
     chunklist = []
-    for j in range(1, popsize+1, 2):
-        #chunk =[]
+    for j in range(1, (popsize*2)+1, 2):
         chunklist.append(j)
-        #chunk.append(j+2)
-        #chunklist.append(chunk)
     for coord in chunklist:
         seq =''
         key=''
-        #filehandle = open(filename)
-        #fc = filehandle.readlines()[coord[0]:coord[1]]
-        print(coord)
         filehandle = open(filename)
         key=filehandle.readlines()[coord].rstrip()
         filehandle.close()
         filehandle = open(filename)
         seq=list(filehandle.readlines()[coord+1].rstrip())
         filehandle.close()
-
-
         allpos=[]
         allpos = ([i for i,val in enumerate(seq) if val=='-' or val=='N'])
         for pos in allpos:
@@ -92,9 +76,7 @@ def get_core(filename, percentcore, popsize):
                 indexdict[pos] = indexdict.get(pos)+1
             else:
                 indexdict[pos] = 1
-        print(f'Sequence {key} finished')
-
-
+        print(f'Sequence {key} added to core')
     threshold = round((popsize/100)*percentcore)
     cutoff = (popsize-threshold)
     thresholdgapindex = []
@@ -106,44 +88,41 @@ def get_core(filename, percentcore, popsize):
     return(thresholdgapindex)
 
 def remove_noncore(filename, thresholdgapindex):
-
+    coreseqindex ={}
     print(f'Number of non-core sites: {len(thresholdgapindex)}')
     print('Deleting non-core sites')
     outname = 'Coresites.fasta'
     coreout=open(outname, 'w')
-
     def delete_noncore(mylist, idx):
         myarray = np.array(mylist)
         mask = np.full(len(mylist), True, dtype=bool)
         mask[idx] = False
         complement = list(myarray[mask])
         return(complement)
-
     chunklist = []
-    for j in range(1, popsize+1, 2):
+    for j in range(1, (popsize*2)+1, 2):
         chunklist.append(j)
+    counter =1
     for coord in chunklist:
         seq =''
         key=''
-
         filehandle = open(filename)
         key=filehandle.readlines()[coord].rstrip()
         filehandle.close()
         filehandle = open(filename)
         seq=list(filehandle.readlines()[coord+1].rstrip())
         filehandle.close()
-
-
+        coreseqindex[key]=counter
         coreseq = ''.join(delete_noncore(seq, thresholdgapindex))
         coreout.write(key+'\n')
         coreout.write(coreseq+'\n')
-        print(f'{key} done (sites deleted)')
+        counter+=2
+        print(f'{key} core seq output')
+    return coreseqindex
 
 if __name__=='__main__':
 
     args = add_args(sys.argv[1:])
-
-
     fn = args.alignment
     fh = open(fn)
     fc = fh.readlines()
@@ -159,63 +138,27 @@ if __name__=='__main__':
     percentcore = args.cutoff
     output.close()
     fc.clear()
-    print('Finished alignment format conversion, to one line per sequence')
-
-    #fh1l = open(outname)
-    #fc1l = fh1l.readlines()
-    #fc1l = fc1l[0:21]
-
-    #Make sequence dictionary: name[seq]
-    #allcols = []
-    #allseqs = []
-    #allseqsdict ={}
-    #for line in fc1l:
-    #    if line.startswith('>'):
-    #        lineindex = fc1l.index(line)
-    #        line = line.rstrip()
-    #        if args.keepref==True:
-    #            allcols.append(line.replace('>',''))
-    #            nextline = fc1l[lineindex+1].rstrip()
-    #            #allseqs.append(list(nextline))
-    #            allseqsdict[line] = list(nextline)
-    #        else:
-    #            if line !='>Reference':
-    #                allcols.append(line.replace('>',''))
-    #                nextline = fc1l[lineindex+1].rstrip()
-    #                #allseqs.append(list(nextline))
-    #                allseqsdict[line] = list(nextline)
-    #fc1l.clear()
-    #fh1l.close()
-
-    #print(f'Extracting ≥{percentcore}% core sites')
-    #allseqsdictcore = get_core(allseqsdict, percentcore)
-    #allseqsdict.clear()
-
+    print('Finished alignment format conversion')
+    print(f'Processing core genome for {popsize} genomes')
+    #Run get-core and output core alignment
     gapindex = get_core(outname, percentcore, popsize)
-    remove_noncore(outname, gapindex)
-
-    exit()
-
+    coreseqindex = remove_noncore(outname, gapindex)
     if args.nodists ==True:
         print('Finished')
         exit()
     else:
-        def get_all_pairs(genomeIDlist):
-            allIDpairs = [",".join(map(str, comb)) for comb in combinations(genomeIDlist, 2)]
-            return(allIDpairs)
-        pairslist = get_all_pairs(allseqsdictcore.keys())
-
-        print('Running pairwise')
-        outname = f'rSNP{percentcore}.csv'
-        output=open(outname, 'w')
+        print('Running pairwise core sequence comparisons')
+        pairslist = [",".join(map(str, comb)) for comb in combinations(coreseqindex.keys(), 2)]
+        pwoutname = f'rSNP{percentcore}.csv'
+        pwoutput=open(pwoutname, 'w')
+        pwoutput.write(f'g1,g2,SNPs,sharedseq,SNPdistance,adjustedSNPs\n')
         nproc = args.nproc
         chunk_size = int(len(pairslist)/nproc)
         chunks = []
         for i in range(0, len(pairslist), chunk_size):
             chunks.append(pairslist[i:i+chunk_size])
-
         with concurrent.futures.ProcessPoolExecutor(nproc) as executor:
-            results = executor.map(get_pw_SNPs, chunks, repeat(percentcore), repeat(len(allseqsdictcore.keys())), repeat(allseqsdictcore))
-            for fSNPs in results:
-                for fSNPline in fSNPs:
-                    output.write(fSNPline)
+            results = executor.map(get_pw_SNPs, chunks, repeat(popsize), repeat(coreseqindex))
+            for SNPs in results:
+                for SNPline in SNPs:
+                    pwoutput.write(SNPline)
