60d528387e0fdc5b6189919e48ba0fcd355d712e
iacobo <47821203+iacobo@users.noreply.github.com>
2022-06-27 13:14:19 +0100

    Add requirements.
    - Add requirements / conda env file.
    - Bit of tidying up in Readme.
    - Linting python file.
    - Cleaning up unnecessary imports, moving internally defined functions outside etc.

--- FlexCore.py
+++ FlexCore.py
@@ -1,169 +1,238 @@
 #!/usr/bin/env python3
-import os, sys, argparse, random
-import numpy as np
-import pandas as pd
-from itertools import combinations
-from itertools import repeat
-import concurrent.futures
+import sys
+import argparse
+from itertools import repeat, combinations
 from concurrent.futures import ProcessPoolExecutor
+
 import screed
+import numpy as np
+
 
 def add_args(a):
-    parser = argparse.ArgumentParser(description=''' Test description ''')
-    parser.add_argument('--alignment', '-a', help='Provide path and filename of alignment', required=True)
-    parser.add_argument('--cutoff', '-c', help='Per-site percent core (integer). Default=95(%)', type=int, default=95)
-    parser.add_argument('--nproc', '-p', help='Number of processes. Default: 1. ', type=int, default=1)
-    parser.add_argument('--nodists', help='Dont calculate SNP distances, only output core alignment. Default: False', required=False, action='store_true')
-    parser.add_argument('--keepref', help='Retain the reference sequence in the core calculation and SNP distances. Default: False', required=False, action='store_true')
+    parser = argparse.ArgumentParser(description=""" Test description """)
+    parser.add_argument(
+        "--alignment",
+        "-a",
+        help="Provide path and filename of alignment",
+        required=True,
+    )
+    parser.add_argument(
+        "--cutoff",
+        "-c",
+        help="Per-site percent core (integer). Default=95(%)",
+        type=int,
+        default=95,
+    )
+    parser.add_argument(
+        "--nproc", "-p", help="Number of processes. Default: 1. ", type=int, default=1
+    )
+    parser.add_argument(
+        "--nodists",
+        help="Dont calculate SNP distances, only output core alignment. Default: False",
+        required=False,
+        action="store_true",
+    )
+    parser.add_argument(
+        "--keepref",
+        help="Retain the reference sequence in the core calculation and SNP distances. Default: False",
+        required=False,
+        action="store_true",
+    )
+
     args = parser.parse_args(a)
-    return(args)
+    return args
+
+
+def delete_not_pw_shared(mylist, idx):
+    myarray = np.array(mylist)
+    mask = np.full(len(mylist), True, dtype=bool)
+    mask[idx] = False
+    complement = list(myarray[mask])
+    return complement
+
 
 def get_pw_SNPs(pairs, popsize, coreseqindex):
-    results =[]
-    print('Running pairwise...')
-    def delete_not_pw_shared(mylist, idx):
-        myarray = np.array(mylist)
-        mask = np.full(len(mylist), True, dtype=bool)
-        mask[idx] = False
-        complement = list(myarray[mask])
-        return(complement)
+    results = []
+    print("Running pairwise...")
+
     for pair in pairs:
-        g1 = pair.split(',')[0]
-        g2 = pair.split(',')[1]
+        g1 = pair.split(",")[0]
+        g2 = pair.split(",")[1]
         g1line = coreseqindex.get(g1)
         g2line = coreseqindex.get(g2)
-        filehandle = open('Coresites.fasta')
-        g1seqlist=list(filehandle.readlines()[g1line].rstrip())
+        filehandle = open("Coresites.fasta")
+        g1seqlist = list(filehandle.readlines()[g1line].rstrip())
         filehandle.close()
-        filehandle = open('Coresites.fasta')
-        g2seqlist=list(filehandle.readlines()[g2line].rstrip())
+        filehandle = open("Coresites.fasta")
+        g2seqlist = list(filehandle.readlines()[g2line].rstrip())
         filehandle.close()
-        #get sites missing in either seq of pair
-        gapindex = list(set([i for i, e in enumerate(g1seqlist) if e == '-'] + [i for i, e in enumerate(g2seqlist) if e == '-']))
-        gapindex += list(set([i for i, e in enumerate(g1seqlist) if e == 'N'] + [i for i, e in enumerate(g2seqlist) if e == 'N']))
-        if len(gapindex)>0:
+        # get sites missing in either seq of pair
+        gapindex = list(
+            set(
+                [i for i, e in enumerate(g1seqlist) if e == "-"]
+                + [i for i, e in enumerate(g2seqlist) if e == "-"]
+            )
+        )
+        gapindex += list(
+            set(
+                [i for i, e in enumerate(g1seqlist) if e == "N"]
+                + [i for i, e in enumerate(g2seqlist) if e == "N"]
+            )
+        )
+
+        if len(gapindex) > 0:
             g1nuc = delete_not_pw_shared(g1seqlist, gapindex)
             g2nuc = delete_not_pw_shared(g2seqlist, gapindex)
         else:
             g1nuc = g1seqlist
             g2nuc = g2seqlist
+
         SNPcount = len(g1nuc) - sum(x == y for x, y in zip(g1nuc, g2nuc))
-        sharedseq = len(g1nuc)-len(gapindex)
-        SNPdist = SNPcount/len(g1nuc)
-        corSNP = SNPdist*(len(g1seqlist))
-        results.append((f'{g1},{g2},{SNPcount},{sharedseq},{SNPdist},{corSNP}\n'))
-        #print(f'{g1},{g2},{SNPcount},{sharedseq},{fSNP}\n')
-    return(results)
+        sharedseq = len(g1nuc) - len(gapindex)
+        SNPdist = SNPcount / len(g1nuc)
+        corSNP = SNPdist * (len(g1seqlist))
+        results.append((f"{g1},{g2},{SNPcount},{sharedseq},{SNPdist},{corSNP}\n"))
+        # print(f'{g1},{g2},{SNPcount},{sharedseq},{fSNP}\n')
+
+    return results
 
 
 def get_core(filename, percentcore, popsize):
-    indexdict ={}
+    indexdict = {}
     chunklist = []
-    for j in range(0, (popsize*2)+1, 2):
+
+    for j in range(0, (popsize * 2) + 1, 2):
         chunklist.append(j)
+
     for coord in chunklist:
-        seq =''
-        key=''
+        seq = ""
+        key = ""
         filehandle = open(filename)
-        key=filehandle.readlines()[coord].rstrip()
+        key = filehandle.readlines()[coord].rstrip()
         filehandle.close()
         filehandle = open(filename)
-        seq=list(filehandle.readlines()[coord+1].rstrip())
+        seq = list(filehandle.readlines()[coord + 1].rstrip())
         filehandle.close()
-        allpos=[]
-        allpos = ([i for i,val in enumerate(seq) if val=='-' or val=='N'])
+        allpos = []
+        allpos = [i for i, val in enumerate(seq) if val == "-" or val == "N"]
+
         for pos in allpos:
             if pos in indexdict:
-                indexdict[pos] = indexdict.get(pos)+1
+                indexdict[pos] = indexdict.get(pos) + 1
             else:
                 indexdict[pos] = 1
-        print(f'Sequence {key} added to core')
-    threshold = round((popsize/100)*percentcore)
-    cutoff = (popsize-threshold)
+
+        print(f"Sequence {key} added to core")
+
+    threshold = round((popsize / 100) * percentcore)
+    cutoff = popsize - threshold
     thresholdgapindex = []
+
     for key in indexdict:
         presence = indexdict.get(key)
+
         if presence > cutoff:
             thresholdgapindex.append(int(key))
+
     indexdict.clear()
-    return(thresholdgapindex)
+
+    return thresholdgapindex
+
+
+def delete_noncore(mylist, idx):
+    myarray = np.array(mylist)
+    mask = np.full(len(mylist), True, dtype=bool)
+    mask[idx] = False
+    complement = list(myarray[mask])
+    return complement
+
 
 def remove_noncore(filename, thresholdgapindex):
-    coreseqindex ={}
-    print(f'Number of non-core sites: {len(thresholdgapindex)}')
-    print('Deleting non-core sites')
-    outname = 'Coresites.fasta'
-    coreout=open(outname, 'w')
-    def delete_noncore(mylist, idx):
-        myarray = np.array(mylist)
-        mask = np.full(len(mylist), True, dtype=bool)
-        mask[idx] = False
-        complement = list(myarray[mask])
-        return(complement)
+    coreseqindex = {}
+    print(f"Number of non-core sites: {len(thresholdgapindex)}")
+    print("Deleting non-core sites")
+    outname = "Coresites.fasta"
+    coreout = open(outname, "w")
+
     chunklist = []
-    for j in range(0, (popsize*2)+1, 2):
+
+    for j in range(0, (popsize * 2) + 1, 2):
         chunklist.append(j)
-    counter =1
+
+    counter = 1
+
     for coord in chunklist:
-        seq =''
-        key=''
+        seq = ""
+        key = ""
         filehandle = open(filename)
-        key=filehandle.readlines()[coord].rstrip()
+        key = filehandle.readlines()[coord].rstrip()
         filehandle.close()
         filehandle = open(filename)
-        seq=list(filehandle.readlines()[coord+1].rstrip())
+        seq = list(filehandle.readlines()[coord + 1].rstrip())
         filehandle.close()
-        coreseqindex[key]=counter
-        coreseq = ''.join(delete_noncore(seq, thresholdgapindex))
-        coreout.write(key+'\n')
-        coreout.write(coreseq+'\n')
-        counter+=2
-        print(f'{key} core seq output')
+        coreseqindex[key] = counter
+        coreseq = "".join(delete_noncore(seq, thresholdgapindex))
+        coreout.write(key + "\n")
+        coreout.write(coreseq + "\n")
+        counter += 2
+        print(f"{key} core seq output")
+
     return coreseqindex
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
 
     args = add_args(sys.argv[1:])
     fn = args.alignment
-    outname = f'{fn}.1l'
-    output = open(outname, 'w')
-    popsize=0
+    outname = f"{fn}.1l"
+    output = open(outname, "w")
+    popsize = 0
+
     for record in screed.open(fn):
-        if args.keepref ==False:
-            if record.name=='Reference':
+        if args.keepref == False:
+            if record.name == "Reference":
                 pass
             else:
-                output.write('>'+record.name+'\n')
-                output.write(record.sequence+'\n')
-                popsize+=1
+                output.write(">" + record.name + "\n")
+                output.write(record.sequence + "\n")
+                popsize += 1
         else:
-            output.write('>'+record.name+'\n')
-            output.write(record.sequence+'\n')
-            popsize+=1
+            output.write(">" + record.name + "\n")
+            output.write(record.sequence + "\n")
+            popsize += 1
 
     percentcore = args.cutoff
     output.close()
-    print('Finished alignment format conversion')
-    print(f'Processing core genome for {popsize} genomes')
-    #Run get-core and output core alignment
+    print("Finished alignment format conversion")
+    print(f"Processing core genome for {popsize} genomes")
+
+    # Run get-core and output core alignment
     gapindex = get_core(outname, percentcore, popsize)
     coreseqindex = remove_noncore(outname, gapindex)
-    if args.nodists ==True:
-        print('Finished')
+
+    if args.nodists == True:
+        print("Finished")
         exit()
     else:
-        print('Running pairwise core sequence comparisons')
-        pairslist = [",".join(map(str, comb)) for comb in combinations(coreseqindex.keys(), 2)]
-        pwoutname = f'rSNP{percentcore}.csv'
-        pwoutput=open(pwoutname, 'w')
-        pwoutput.write(f'g1,g2,SNPs,sharedseq,SNPdistance,adjustedSNPs\n')
+        print("Running pairwise core sequence comparisons")
+        pairslist = [
+            ",".join(map(str, comb)) for comb in combinations(coreseqindex.keys(), 2)
+        ]
+        pwoutname = f"rSNP{percentcore}.csv"
+        pwoutput = open(pwoutname, "w")
+        pwoutput.write(f"g1,g2,SNPs,sharedseq,SNPdistance,adjustedSNPs\n")
         nproc = args.nproc
-        chunk_size = int(len(pairslist)/nproc)
+        chunk_size = int(len(pairslist) / nproc)
         chunks = []
+
         for i in range(0, len(pairslist), chunk_size):
-            chunks.append(pairslist[i:i+chunk_size])
-        with concurrent.futures.ProcessPoolExecutor(nproc) as executor:
-            results = executor.map(get_pw_SNPs, chunks, repeat(popsize), repeat(coreseqindex))
+            chunks.append(pairslist[i : i + chunk_size])
+
+        with ProcessPoolExecutor(nproc) as executor:
+            results = executor.map(
+                get_pw_SNPs, chunks, repeat(popsize), repeat(coreseqindex)
+            )
+
             for SNPs in results:
                 for SNPline in SNPs:
                     pwoutput.write(SNPline)
--- README.md
+++ README.md
@@ -1,44 +1,52 @@
 # FlexCore
+
 Extract per-site flexible (default ≥95%) bacterial core genomes from read-mapped whole genome alignments. Optionally calculate core genome SNP counts and distances.
 
-For 100% site inclusion core genomes I highly recommend instead using snp-sites (https://github.com/sanger-pathogens/snp-sites)
-for overall speed and performance, followed by snp-dists (https://github.com/tseemann/snp-dists). 
+For 100% site inclusion core genomes I highly recommend instead using [snp-sites](https://github.com/sanger-pathogens/snp-sites)
+for overall speed and performance, followed by [snp-dists](https://github.com/tseemann/snp-dists).
 
 ## Dependencies
 
-FlexCore.py is written in python3 and requires the following python packages: 
->os; sys; argparse; random; numpy; pandas; itertools; concurrent; screed
+FlexCore.py is written in python3 and requires the following python packages:
+> numpy; screed
 
 ## Usage
-	python FlexCore.py --alignment [alignment file]
+
+```shell
+python FlexCore.py --alignment [alignment file]
+```
 
 ## Input
-Multi-fasta whole genome alignment derived from mapping to a reference and variant calling such as from snippy, snippy-core and snippy-clean_full_aln:
 
-	snippy-core --ref ref.fa snippyoutfiles 
-	snippy-clean_full_aln core.full.aln > clean.full.aln
-https://github.com/tseemann/snippy
+Multi-fasta whole genome alignment derived from mapping to a reference and variant calling such as from [snippy](https://github.com/tseemann/snippy), snippy-core and snippy-clean_full_aln:
+
+```shell
+snippy-core --ref ref.fa snippyoutfiles 
+snippy-clean_full_aln core.full.aln > clean.full.aln
+```
 
 ## Output
-*Coresites.csv*, the core genome alignment  
-*rSNPs95.csv*, the comma separated non-redundant (exclusive) pairwise distances (optional)
+
+- *Coresites.csv*, the core genome alignment  
+- *rSNPs95.csv*, the comma separated non-redundant (exclusive) pairwise distances (optional)
 
 >SNP distances are calculated by the number of comparable sites per pair in the core genome alignment. Though a core genome cutoff for the total core alignment may be ≥95%, in principle any number of sites may be missing between a pair of sequences (they may have far fewer sites without gaps or ambiguous bases than the overall alignment size). As such, calculating SNP distance by dividing by the total alignment length may be innapropriate. The SNP counts, SNP distance (SNP count/length of pairwise alignment) and adjusted SNP counts are output (SNP distance times by overall alignment length)
 
-
 ## Options
 
+```shell
+'--alignment', '-a', help='Provide path and filename of alignment',                  required=True
+'--cutoff',    '-c', help='Per-site percent core (integer). Default=95(%)',          type=int, default=95
+'--nproc',     '-p', help='Number of processes;                                      type=int, default=1
+'--nodists',         help='Dont calculate SNP distances, only output core alignment;                action='store_true'
+'--keepref',         help='Retain the reference sequence in the core calculation and SNP distances; action='store_true' 
+```
 
-	'--alignment', '-a', help='Provide path and filename of alignment', required=True
-	'--cutoff', '-c', help='Per-site percent core (integer). Default=95(%)', type=int, default=95
-	'--nproc', '-p', help='Number of processes; type=int, default=1
-    '--nodists', help='Dont calculate SNP distances, only output core alignment; action='store_true'
-	'--keepref', help='Retain the reference sequence in the core calculation and SNP distances; action='store_true' 
-
-The --keepref option expects the reference file to be named '>Reference' in line with snippy-core output
+The `--keepref` option expects the reference file to be named '>Reference' in line with snippy-core output
 
 ## Similar software
+
 Other software (that I'm aware of) for extracting flexible core genomes following reference mapping and variant calling:
 
-https://github.com/davideyre/runListCompare  
-https://github.com/zheminzhou/EToKi ('align' or 'phylo' module)
+- https://github.com/davideyre/runListCompare  
+- https://github.com/zheminzhou/EToKi ('align' or 'phylo' module)
